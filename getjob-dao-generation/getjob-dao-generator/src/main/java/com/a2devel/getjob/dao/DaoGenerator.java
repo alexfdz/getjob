dfbabaaf9f678b772edb2e19a049091dbc3aa7c1
Alex Fernandez <alexfdz@gmail.com>
2012-05-19 00:17:43 +0100

    Schema definition

--- getjob-dao-generation/getjob-dao-generator/src/main/java/com/a2devel/getjob/dao/DaoGenerator.java
+++ getjob-dao-generation/getjob-dao-generator/src/main/java/com/a2devel/getjob/dao/DaoGenerator.java
@@ -1,7 +1,10 @@
 package com.a2devel.getjob.dao;
 
 import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
 import de.greenrobot.daogenerator.Schema;
+import de.greenrobot.daogenerator.ToMany;
+import de.greenrobot.daogenerator.ToOne;
 
 /**
  * 
@@ -13,17 +16,33 @@ public class DaoGenerator {
     public static void main(String[] args) throws Exception {
         Schema schema = new Schema(Integer.parseInt(args[0]), args[1]);
 
-        addNote(schema);
+        defineSchema(schema);
 
         new de.greenrobot.daogenerator.DaoGenerator().generateAll(schema, args[2]);
     }
 
-    private static void addNote(Schema schema) {
-        Entity note = schema.addEntity("Note");
-        note.addIdProperty();
-        note.addStringProperty("text").notNull();
-        note.addStringProperty("comment");
-        note.addDateProperty("date");
+    private static void defineSchema(Schema schema) {
+    	Entity search = schema.addEntity("Search");
+    	search.addIdProperty();
+    	search.addStringProperty("url").notNull();
+    	search.addStringProperty("search").notNull();
+    	search.addDateProperty("timestamp").notNull();
+    	
+    	Entity result = schema.addEntity("Result");
+        Property resultId = result.addIdProperty().getProperty();
+        result.addStringProperty("text").notNull();
+        result.addStringProperty("url").notNull();
+        result.addDateProperty("timestamp").notNull();
+        result.addShortProperty("favorite");
+        result.addShortProperty("applied");
+        
+        Property resultSearchId = result.addLongProperty("searchId").notNull().getProperty();
+        ToOne resultToSearch = result.addToOne(search, resultSearchId);
+        resultToSearch.setName("search");
+        
+        ToMany searchToResults = search.addToMany(result, resultSearchId);
+        searchToResults.setName("results");
+        searchToResults.orderAsc(resultId);
     }
 
 }
